07924e2dfaf819ad7d70fc5927fb731219004dac
Radek <radoslaw.biesek@gmail.com>
2019-02-11 18:39:17 +0100

    Create sample pug templates

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 
 app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use('/store', function(req, res, next) {
     console.log('Jestem pośrednikiem przy żądaniu do /store');
@@ -14,6 +16,30 @@ app.get('/', function(req, res) {
 app.get('/store', function(req, res) {
     res.send('To jest sklep');
 });
+
+app.get('/first-template', function(req, res) {
+    res.render('first-template');
+}); 
+
+app.get('/dynamic-view', function(req, res) {
+    res.render('dynamic-view', {
+        name: 'Moja dynamiczna strona',
+        url: 'http://google.com'
+    });
+});
+
+app.get('/dynamic-view2', function(req, res) {
+    res.render('dynamic-view2', {
+        user: {
+            name: 'Radek',
+            age: "27"
+        }
+    });
+});
+
+app.get('/content', function(req, res) {
+    res.render('content');
+})
 
 app.listen(3000);
 app.use(function(req, res, next) {
